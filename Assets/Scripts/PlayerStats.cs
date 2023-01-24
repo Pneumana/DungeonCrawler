5936a927a8c8561ff39b2bec7e8a2465ef32a17b
Pneumana <connor_nicewarner1@my.vcccd.edu>
2023-01-24 12:25:25 -0800

    added project settings so i dont have to mess around with physics 2D settings every time

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.Localization
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        public int Area;
+        public int Speed;
+        public int Haste;
+        public int Damage;
+        public int Pierce;
+        public int Health;
+    }
+}
